ec977cb67b5404e8a329bc09cdd8591b51be473b
jaspreet kaur <jaspreetkaur17@academic.rrc.ca>
2024-07-07 23:34:26 -0500

    added validations to book model for presence

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,3 +1,9 @@
 class Story < ApplicationRecord
   belongs_to :book
 end
+
+
+# validations
+validations :title, presence; true
+validations :content, presence; true
+validations :author, presence; true
