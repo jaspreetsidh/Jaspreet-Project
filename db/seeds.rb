07a571e5de414fb9848b29b0d2ff3e1156077de7
jaspreet kaur <jaspreetkaur17@academic.rrc.ca>
2024-07-07 23:42:02 -0500

    Adding  seed data done  to populate the books

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,23 @@ require 'faker'
 Book.destroy_all
 Story.destroy_all
 
+# Seed data from books.csv
+CSV.foreach(Rails.root.join('db', 'books.csv'), headers: true) do |row|
+  Book.create!(
+    bookID: row['bookID'],
+    title: row['title'],
+    authors: row['authors'],
+    average_rating: row['average_rating'],
+    isbn: row['isbn'],
+    isbn13: row['isbn13'],
+    language_code: row['language_code'],
+    num_pages: row['num_pages'],
+    ratings_count: row['ratings_count'],
+    text_reviews_count: row['text_reviews_count'],
+    publication_date: Date.strptime(row['publication_date'], '%m/%d/%Y'),
+    publisher: row['publisher']
+  )
+end
 
 # Seed data from Faker for Stories
 Book.all.each do |book|
