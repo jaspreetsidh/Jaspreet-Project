9a9926b5038e3612756695d22196288a0f0d688f
jaspreet kaur <jaspreetkaur17@academic.rrc.ca>
2024-07-07 23:59:49 -0500

    pagination done for 10 pages

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+  def index
+    if params[:search].present?
+      @books_with_stories = Book.includes(:stories).where("books.title LIKE ?", "%#{params[:search]}%")
+      @stories = Story.includes(:book).where("stories.title LIKE ?", "%#{params[:search]}%")
+    else
+      @books_with_stories = Book.includes(:stories).all
+      @stories = Story.all
+    end
+
+    @books_with_stories = @books_with_stories.paginate(page: params[:page], per_page: 10)
+  end
+end
