936fa4b5db87ac8b917cf0a922df3db8f5ba689e
jaspreet kaur <jaspreetkaur17@academic.rrc.ca>
2024-07-08 00:24:58 -0500

    updated validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,16 +2,3 @@ class Book < ApplicationRecord
     has_many :stories, dependent: :destroy
 end
 
-
-# validations
-validations :title, presence; true
-validations :author, presence; true
-validations :average_rating, presence; true
-validations :isbn, presence; true
-validations :isbn13, presence; true
-validations :language_code, presence; true
-validations :num_pages, presence; true
-validations :ratings_count, presence; true
-validations :text_reviews_count, presence; true
-validations :publication_date, presence; true
-validations :publisher, presence; true
